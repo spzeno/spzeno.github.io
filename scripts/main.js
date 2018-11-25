0948f0b5e6688584262cfca49c2461d8fbc72eec
spzeno <spzeno@163.com>
2018-11-26 00:35:15 +0800

    test

--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,19 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/3.jpg') {
-      myImage.setAttribute('src', 'images/1.jpg');
+	var next = document.getElementById("nImg");
+	
+	
+	
+    /*if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src', 'images/firefox2.png');
     } else {
-      myImage.setAttribute('src', 'images/3.jpg');
+      myImage.setAttribute('src', 'images/firefox-icon.png');
     }
-}
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Hello, ' + myName;
-}
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Hello, ' + storedName;
-}
-myButton.onclick = function() {
-  setUserName();
+	
+	
+	
+	
+	
+    myImage.setAttribute('src', 'images/'+);*/
 }
